a7777f1fadad0892db89216268e19c525df71cc5
REIGaruki <gyakujo@mail.ru>
2024-04-12 15:20:15 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        int value = 33; // объявляем переменную value
+        changeValue(value); // применяем метод
+        System.out.println(value); // в консоль выводится "33"
+    }
+    public static void changeValue(int value) {
+        // внутрь медота changeValue подается переменная value из метода main
+        value = 22; // далее медот работает уже с собственной переменной value, значение которой взято из
+        // переменной value метода main. Ей присваивается значение "22", после чего метод завершается
+        // и переменная value удаляется из памяти. Среда разработки выделяет внутреннюю переменную value
+        // метода changeValue серым цветом, показывая что в методе main она использоваться не будет.
     }
 }
